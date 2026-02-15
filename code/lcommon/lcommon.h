8607b793834c7dfe8a1b0a113448e6e259192979
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-02-15 16:52:34 +0100

    Finished the lair common file as well, now onto the implementation part. Following Carmack patterns, I will also use similar things for building the server and establishing connections between a user and the server right.

--- code/lcommon/lcommon.h
+++ code/lcommon/lcommon.h
@@ -9,4 +9,32 @@
 
 
 
-#endif
+/*
+==============================================================
+COMMON FUNCTIONS (lcommon.h)
+==============================================================
+*/
+
+
+/* Printing */
+void Com_Printf( const char *fmt, ... );
+
+
+void Com_DPrintf( const char *fmt, ... );
+
+
+/* Errors */
+void Com_Error( lerror_t level, const char *fmt, ... );
+
+
+/* Initialization */
+void Com_Init( void );
+void Com_Shutdown( void );
+
+
+/* Time */
+u64 Com_Milliseconds( void );
+
+
+
+#endif        /* __LAIR_COMMON_H END */
