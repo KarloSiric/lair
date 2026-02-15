142a3d06bdbdd4214509b3466eef81c22d97802d
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-02-15 17:05:56 +0100

    Added lair common files and the platform so that they are ready to include into posix first implementing things right

--- code/lcommon/platform.h
+++ code/lcommon/platform.h
@@ -17,34 +17,45 @@
 */
 
 
-
 int Sys_Init( void );
-
-
 
 void Sys_Shutdown( void );
 
+void Sys_Print( const char *msg );
 
+void Sys_Error( const char *msg );
 
 u64 Sys_GetMilliseconds( void );
 
 
 
+/*
+ ======================================
+            NETWORK ( Net_ )
+ ======================================
+*/
+
+
+void Net_Init( void );
+
+void Net_Shutdown( void );
+
+lsocket Net_Socket( void );
+
+int Net_Bind( lsocket sock, u16 port );
+
+int Net_Listen( lsocket sock, int backlog );
+
+lsocket Net_Accept( lsocket sock );
+
+int Net_Connect( lsocket sock, const char *host, u16 port );
+
+int Net_Send( lsocket sock, byte *buf, usize buflen );
+
+int Net_Recv( lsocket sock, byte *buf, usize buflen );
+
+void Net_Close( lsocket sock );
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif
+#endif             /* __LAIR_PLATFORM_H END */
