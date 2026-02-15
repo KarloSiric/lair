8ffc42b1e3d67a4728058b56511a7f9c0917549f
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-02-15 01:31:17 +0100

    Adding new values for the types lair

--- core/include/lair/types.h
+++ core/include/lair/types.h
@@ -10,6 +10,7 @@
 #include <stdint.h>
 
 
+/* Unsigned integers */
 
 typedef uint8_t  u8;
 typedef uint16_t u16;
@@ -20,9 +21,22 @@ typedef uint64_t u64;
 
 /* Signed integers */
 
-typedef int8_t   i8;
-typedef int16_t  i16;
-typedef int32_t  i32;
+typedef int8_t   s8;
+typedef int16_t  s16;
+typedef int32_t  s32;
+typedef int64_t  s64;
+
+
+/* Floating point values */
+
+typedef float    f32;         // 32 bits pretty much everywhere ( 4 bytes )
+typedef double   f64;         // 64 bits 
+
+
+
+
+
+
 
 
 
